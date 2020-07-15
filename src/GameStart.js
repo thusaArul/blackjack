ac8f4e35a8141d4e225e4b90dcb29096d930d147
thusaArul <thusa.arul@hotmail.com>
2020-07-15 15:14:14 -0400

    fixed some bugs

--- src/GameStart.js
+++ src/GameStart.js
@@ -9,6 +9,7 @@ class GameStart extends Component {
             playersTotal: 0,
             dealersHand: [],
             dealersTotal: 0,
+            dealersHiddenTotal: 0,
             dealersHiddenCard: [],
             roundOver: '',
             newGame: false,
@@ -16,6 +17,7 @@ class GameStart extends Component {
         this.hit = this.hit.bind(this);
         this.stay = this.stay.bind(this);
         this.newGame = this.newGame.bind(this);
+        this.results = this.results.bind(this);
     }
 
     hit() {
@@ -75,96 +77,104 @@ class GameStart extends Component {
 
     stay() {
         if (this.state.roundOver !== 'BUST!') {
-            if (this.state.dealersHand[0].points === 0) {
-                let revealedHand = [...this.state.dealersHand];
-                revealedHand[0].image = this.state.dealersHiddenCard;
-                revealedHand[0].points = +this.state.dealersHiddenValue;
-                this.setState({
-                    dealersTotal: +this.state.dealersTotal + +this.state.dealersHiddenValue,
-                })
+            if (this.state.dealersHiddenValue < 17 && this.state.playersTotal < 22) {
+                if (this.state.dealersHand[0].points === 0) {
+                    let revealedHand = [...this.state.dealersHand];
+                    revealedHand[0].image = this.state.dealersHiddenCard;
+                    revealedHand[0].points = +this.state.dealersHiddenValue;
+                    this.setState({
+                        dealersTotal: +this.state.dealersTotal + +this.state.dealersHiddenValue,
+                    })
+                }
+                if (this.state.dealersHiddenTotal < 17) {
+                    this.dealNewCard();
+                } else {
+                    let stayingHand = [...this.state.dealersHand];
+                    this.setState({
+                        dealersHand: stayingHand,
+                    })
+                    this.results();
+                }
             }
-            if (+this.state.dealersTotal + +this.state.dealersHiddenValue > 16) {
-                console.log('test')
-            }
-            this.dealNewCard();
         }
     }
 
     dealNewCard() {
-        if (this.state.dealersTotal < 17 && this.state.playersTotal < 22) {
-            axios({
-                url: 'https://deckofcardsapi.com/api/deck/' + this.props.deck_id + '/draw/?count=1',
-                method: 'GET',
-                responseType: 'JSON',
-            }).then((response) => {
-                response.data.cards.forEach(card => {
-                    if (
-                        card.value === 'JACK' ||
-                        card.value === 'QUEEN' ||
-                        card.value === 'KING'
-                    ) {
-                        card.points = '10';
-                    } else if (card.value === 'ACE') {
-                        card.points = '11';
-                        card.value = 'ACE (worth 11 points)'
-                    } else (
-                        card.points = card.value
-                    )
-                });
-                let newHand = [...this.state.dealersHand];
-                const newCard = response.data.cards[0];
-                newHand.push(newCard);
-                this.setState({
-                    dealersHand: newHand,
-                    dealersTotal: this.state.dealersTotal + +response.data.cards[0].points,
-                })
-                if (this.state.dealersTotal > 21) {
-                    let i;
-                    for (i = 0; i < this.state.dealersHand.length; i = i + 1) {
-                        if (this.state.dealersHand[i].value === 'ACE (worth 11 points)') {
-                            let newValue = this.state.dealersHand;
-                            newValue[i].value = 'ACE (worth 1 point)'
-                            this.setState({
-                                dealersHand: newValue,
-                                dealersTotal: this.state.dealersTotal - 10,
-                            })
-                        }
-                    }
-                }
-            }).then((response) => {
-                if (this.state.dealersTotal < 17) {
-                    this.dealNewCard();
-                } else if (this.state.dealersTotal > 21) {
-                    console.log('win')
-                    this.setState({
-                        roundOver: 'WIN!!!',
-                        newGame: true,
-                    })
-                } else if (this.state.dealersTotal >= 17) {
-                    if (this.state.playersTotal > this.state.dealersTotal) {
-                        console.log('win')
-                        this.setState({
-                            roundOver: 'WIN!!!',
-                            newGame: true,
-                        })
-                    } else if (this.state.playersTotal < this.state.dealersTotal) {
-                        console.log('lose')
-                        this.setState({
-                            roundOver: 'LOSE...',
-                            newGame: true,
-                        })
-                    } else if (this.state.playersTotal === this.state.dealersTotal) {
-                        console.log('tie')
-                        this.setState({
-                            roundOver: 'TIED.',
-                            newGame: true,
-                        })
-                    }
-                }
+        axios({
+            url: 'https://deckofcardsapi.com/api/deck/' + this.props.deck_id + '/draw/?count=1',
+            method: 'GET',
+            responseType: 'JSON',
+        }).then((response) => {
+            response.data.cards.forEach(card => {
+                if (
+                    card.value === 'JACK' ||
+                    card.value === 'QUEEN' ||
+                    card.value === 'KING'
+                ) {
+                    card.points = '10';
+                } else if (card.value === 'ACE') {
+                    card.points = '11';
+                    card.value = 'ACE (worth 11 points)'
+                } else (
+                    card.points = card.value
+                )
+            });
+            let newHand = [...this.state.dealersHand];
+            const newCard = response.data.cards[0];
+            newHand.push(newCard);
+            this.setState({
+                dealersHand: newHand,
+                dealersTotal: this.state.dealersTotal + +response.data.cards[0].points,
             })
-        }
+            if (this.state.dealersTotal > 21) {
+                let i;
+                for (i = 0; i < this.state.dealersHand.length; i = i + 1) {
+                    if (this.state.dealersHand[i].value === 'ACE (worth 11 points)') {
+                        let newValue = this.state.dealersHand;
+                        newValue[i].value = 'ACE (worth 1 point)'
+                        this.setState({
+                            dealersHand: newValue,
+                            dealersTotal: this.state.dealersTotal - 10,
+                        })
+                    }
+                }
+            }
+        }).then((response) => {
+            this.results();
+        })
     }
 
+    results() {
+        if (this.state.dealersTotal < 17) {
+            this.dealNewCard();
+        } else if (this.state.dealersTotal > 21) {
+            console.log('win')
+            this.setState({
+                roundOver: 'WIN!!!',
+                newGame: true,
+            })
+        } else if (this.state.dealersTotal >= 17) {
+            if (this.state.playersTotal > this.state.dealersTotal) {
+                console.log('win')
+                this.setState({
+                    roundOver: 'WIN!!!',
+                    newGame: true,
+                })
+            } else if (this.state.playersTotal < this.state.dealersTotal) {
+                console.log('lose')
+                this.setState({
+                    roundOver: 'LOSE...',
+                    newGame: true,
+                })
+            } else if (this.state.playersTotal === this.state.dealersTotal) {
+                console.log('tie')
+                this.setState({
+                    roundOver: 'TIED.',
+                    newGame: true,
+                })
+            }
+        }
+    }
 
     newGame() {
         this.setState({
@@ -172,7 +182,9 @@ class GameStart extends Component {
             playersTotal: 0,
             dealersHand: [],
             dealersTotal: 0,
-            dealersHiddenCard: [],
+            dealersHiddenTotal: 0,
+            dealersHiddenCard: '',
+            dealersHiddenValue: 0,
             roundOver: '',
             newGame: false,
         })
@@ -198,11 +210,11 @@ class GameStart extends Component {
             this.setState({
                 dealersHiddenCard: response.data.cards[0].image,
                 dealersHiddenValue: response.data.cards[0].points,
+                dealersHiddenTotal: +response.data.cards[0].points + +response.data.cards[2].points
             })
+            console.log(this.state.dealersHiddenValue)
             let playersStartingHand = [...this.state.playersHand];
             let dealersStartingHand = [...this.state.dealersHand];
-            console.log(this.state.dealersHiddenCard)
-            console.log(this.state.dealersHiddenValue)
             let firstCard = response.data.cards[0];
             firstCard.image = 'https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1462119328';
             firstCard.points = 0;
@@ -212,7 +224,7 @@ class GameStart extends Component {
             dealersStartingHand.push(firstCard, thirdCard);
             playersStartingHand.push(secondCard, fourthCard);
 
-            this.setState({
+            this.setState({ 
                 playersHand: playersStartingHand,
                 dealersHand: dealersStartingHand,
             });
@@ -220,14 +232,34 @@ class GameStart extends Component {
             let i = 0;
             for (i = 0; i < this.state.playersHand.length; i = i + 1) {
                 this.setState({
+                    dealersTotal: response.data.cards[2].points,
+                })
+                this.setState({
                     playersTotal: this.state.playersTotal + +this.state.playersHand[i].points,
                 })
                 if (this.state.playersTotal > 21) {
                     let i;
                     for (i = 0; i < this.state.playersHand.length; i = i + 1) {
                         if (this.state.playersHand[i].points === '11') {
+                            let newValue = this.state.playersHand;
+                            newValue[i].value = 'ACE (worth 1 point)'
                             this.setState({
+                                playersHand: newValue,
                                 playersTotal: this.state.playersTotal - 10,
+                            })
+                        }
+                        return;
+                    }
+                }
+                if (this.state.dealersTotal > 21) {
+                    let i;
+                    for (i = 0; i < this.state.dealersHand.length; i = i + 1) {
+                        if (this.state.dealersHand[i].points === '11') {
+                            let newValue = this.state.dealersHand;
+                            newValue[i].value = 'ACE (worth 1 point)'
+                            this.setState({
+                                dealersHand: newValue,
+                                dealersTotal: this.state.dealersTotal - 10,
                             })
                         }
                         return;
@@ -237,9 +269,6 @@ class GameStart extends Component {
                     this.stay();
                 }
             }
-            this.setState({
-                dealersTotal: response.data.cards[2].points,
-            })
         });
     }
 
